eb193e55f33f6972d06499fc72d430d9852bbb61
Carlos Alberto Giralt Torriente <cagiraltt@gmail.com>
2022-05-24 09:05:27 -0400

    Setup tailwindcss.

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  important: true,
+  purge: {
+    enabled: true,
+    content: [
+      "./src/**/*.{html,scss,ts}",
+    ],
+  },
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
